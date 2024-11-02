6e439329dfa95fc36817c242c6cd41d75b28a26e
yared-Habtamu <yaredhabtamu08@gmail.com>
2024-11-02 09:58:57 +0300

    Retrived

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,3 +4,6 @@ class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.CharField(max_length=100)
     publication_year=models.IntegerField()
+
+    def __str__(self):
+        return self.title
---
+++ Introduction_to_Django/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,2 @@
+Book.objects.all()
+#<QuerySet [<Book: 1984>, <Book: 1984>]>
