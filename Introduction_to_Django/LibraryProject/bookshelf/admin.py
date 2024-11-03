d6d352979bafb969e7c1d920ba1035b55f879a12
Yared <yaredhabtamu08@gmail.com>
2024-11-03 17:41:36 +0300

    Done

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,5 @@ class BookAdmin(models.Model):
     list_display=('title','author','publication_year')
     list_filter=('title')
     search_fields=('title', 'author')
+
+admin.site.register(Book,BookAdmin)
