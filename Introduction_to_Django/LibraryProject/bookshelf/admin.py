dec97ae7390adb6188e85c8468df65d073c56aca
Yared <yaredhabtamu08@gmail.com>
2024-11-03 17:39:11 +0300

    Admin customization configured

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from ,models import Book
 
 # Register your models here.
+
+class BookAdmin(models.Model):
+    list_display=('title','author','publication_year')
+    list_filter=('title')
+    search_fields=('title', 'author')
