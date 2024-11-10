1f1dc627bbb3c2211f2672f13fc84b3a05553499
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-10 16:53:06 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 def list_books(request, books=None):
@@ -12,3 +16,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to home page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'users/logout.html'
