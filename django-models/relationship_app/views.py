0dc848c274cf2f606c3aba054b87d98c4d2f812d
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-10 17:11:01 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 def list_books(request, books=None):
@@ -33,3 +34,21 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     template_name = 'users/logout.html'
+
+#3
+
+def check_role(user, role):
+    return user.userprofile.role == role
+
+@user_passes_test(lambda u: check_role(u, 'Admin'))
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(lambda u: check_role(u, 'Librarian'))
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(lambda u: check_role(u, 'Member'))
+def member_view(request):
+    return render(request, 'member_view.html')
+
