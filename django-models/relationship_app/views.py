770ec339da3fbc94860773e4aa388061bd3834e0
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-10 17:32:25 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -42,13 +42,13 @@ def check_role(user, role):
 
 @user_passes_test(lambda u: check_role(u, 'Admin'))
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(lambda u: check_role(u, 'Librarian'))
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(lambda u: check_role(u, 'Member'))
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
 
