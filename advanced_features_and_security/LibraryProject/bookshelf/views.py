aa320eef8e7032610d1e4bde2eedbce64fd715f8
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 14:54:55 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponseForbidden
+from .models import Book
 
-# Create your views here.
+@permission_required('myapp.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        publication_year = request.POST['publication_year']
+        Book.objects.create(title=title, author=author, publication_year=publication_year)
+        return redirect('book_list')
+    return render(request, 'myapp/create_book.html')
+
+@permission_required('myapp.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.publication_year = request.POST['publication_year']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'myapp/edit_book.html', {'book': book})
+
+@permission_required('myapp.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'myapp/delete_book.html', {'book': book})
+
+@permission_required('myapp.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'myapp/book_list.html', {'books': books})
