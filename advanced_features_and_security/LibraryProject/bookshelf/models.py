1450ceed7f01bb9afc72406a3c1210d1795bef0e
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 14:29:43 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,11 +10,21 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 #week11
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-# accounts/models.py
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-      return self.create_user(email, password, **extra_fields)
-
+        return self.create_user(email, password, **extra_fields)
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
