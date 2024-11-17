b4e19c9e0e69bdb78c46515e524772d64a7643eb
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 21:21:20 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -37,15 +37,3 @@ def delete_book(request, book_id):
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'myapp/book_list.html', {'books': books})
-
-
-#3
-@permission_required('myapp.can_create', raise_exception=True) 
-def create_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid(): form.save()
-            return redirect('book_list')
-    else: form = BookForm()
-        return render(request, 'myapp/create_book.html', {'form': form})
-
