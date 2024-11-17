9f24bccea3f80b54db47698ee6125f5a437bf9de
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 21:16:01 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 # Create your models here.
