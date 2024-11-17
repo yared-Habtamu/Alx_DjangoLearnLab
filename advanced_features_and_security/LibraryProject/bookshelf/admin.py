e27c486eaa46d3fe2554f12d4289be4130414f69
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 14:32:11 +0300

    registered

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from . import models
 
 # Register your models here.
 
@@ -9,3 +10,4 @@ class BookAdmin(admin.ModelAdmin):
     search_fields=('title', 'author')
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
