67421dc4ef0f2416f39b6a70a5c03488ae229848
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 15:11:44 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,6 @@
+from django import forms
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200, label='Book Title')
+    author = forms.CharField(max_length=100, label='Author Name')
+    publication_year = forms.IntegerField(label='Publication Year')
