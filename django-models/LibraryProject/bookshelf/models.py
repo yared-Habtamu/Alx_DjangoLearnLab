21dcb59d04daef19091c464f14219b2a52300e21
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 14:04:09 +0300

    Update models.py

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
 # Create your models here.
 class Book(models.Model):
     title=models.CharField(max_length=200)
@@ -7,3 +9,19 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+#week11
+
+# accounts/models.py
+
+      return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
