c4f1897c82b3be61d50558cb72bee56f978e0236
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-11-17 15:15:54 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseForbidden
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('myapp.can_create', raise_exception=True)
 def create_book(request):
