c509e899f2162b34d040d58f817cd218bf2588e4
Yared <yaredhabtamu08@gmail.com>
2024-11-24 12:28:38 +0300

    Tested

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -129,5 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
