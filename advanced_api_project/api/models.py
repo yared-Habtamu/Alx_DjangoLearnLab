9bba80dfafb575cb8bd6ba14c435e900ee73fe41
Yared <yaredhabtamu08@gmail.com>
2024-12-01 00:04:40 +0300

    Done

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
