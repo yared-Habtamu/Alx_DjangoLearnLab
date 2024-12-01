d98222d99c7358bc147e8b3c6921517b1de6686f
Yared <yaredhabtamu08@gmail.com>
2024-12-01 17:23:55 +0300

    Annoying Variable name

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
