fa460974076cccab853d41934eca30e46ac4d1a5
Yared <yaredhabtamu08@gmail.com>
2024-12-06 23:08:00 +0300

    Task2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm, User
+
 
 # Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        return render(request, 'blog/register.html', {'form': form})
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after successful registration
+        return render(request, 'blog/register.html', {'form': form})
+
+
+class ProfileUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        form = ProfileUpdateForm(instance=request.user)
+        return render(request, 'blog/profile.html', {'form': form})
+
+    def post(self, request):
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        return render(request, 'blog/profile.html', {'form': form})
