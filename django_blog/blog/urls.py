7482095c54624392dd3b9e19280e423591d046b6
Yared <yaredhabtamu08@gmail.com>
2024-12-08 00:21:42 +0300

    Tried

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('post/', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
