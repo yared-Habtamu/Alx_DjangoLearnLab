2cc3d0828faaa4befe1a6baf5d186fae255e84f4
Yared <yaredhabtamu08@gmail.com>
2024-12-26 16:50:38 +0300

    unnecessary files deleted

---
+++ Chess/Chesss/Time.java
@@ -0,0 +1,11 @@
+import java.util.Timer;
+
+public class Timer {
+    private int whiteTime;
+    private int blackTime;
+
+    public Timer(int timeLimit) {
+        this.whiteTime = timeLimit;
+        this.blackTime = timeLimit;
+    }
+}
